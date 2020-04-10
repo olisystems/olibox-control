70ba3dccee86669c1211bd0d1e3b15382738f48b
m-yahya <yahya_gis@hotmail.com>
2020-04-10 07:06:21 +0200

    Update function to add data to respective key

--- olibox_control/control_pkg/helpers.py
+++ olibox_control/control_pkg/helpers.py
@@ -12,11 +12,15 @@ def write_values(file, new_data):
     if os.path.isfile(file_to_write):
         with open(file_to_write) as f:
             data = json.load(f)
+            # get existing data keys
+            keys = data.keys()
+            for k in keys:
+                # update keys only if they exists in new data
+                if k in new_data.keys():
+                    data[k].update(new_data[k])
 
-        data.update(new_data)
-        with open(file_to_write, 'w+', encoding='utf-8') as f:
-            json.dump(data, f, ensure_ascii=False, indent=4)
+            with open(file_to_write, 'w+', encoding='utf-8') as f:
+                json.dump(data, f, ensure_ascii=False, indent=4)
     else:
         with open(file_to_write, 'w+', encoding='utf-8') as f:
             json.dump(new_data, f, ensure_ascii=False, indent=4)
-
